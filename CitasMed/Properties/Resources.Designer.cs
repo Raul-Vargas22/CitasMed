42bca74877fe0f744c46c9feb47169348a54ac0c
Yadira Acosta <acostayadira194-glitch>
2026-07-09 11:24:23 -0600

    Hice los cmabios de los formularios

--- CitasMed/Properties/Resources.Designer.cs
+++ CitasMed/Properties/Resources.Designer.cs
@@ -149,5 +149,15 @@ namespace CitasMed.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Captura_de_pantalla_2026_07_09_031633 {
+            get {
+                object obj = ResourceManager.GetObject("Captura de pantalla 2026-07-09 031633", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
