ea3fb8ea322a0967827e34f5760260dc0a73d8fb
Lander Javier Serna Hernandez <sernalander11@gmail.com>
2026-07-09 22:11:36 -0600

    Se agregó cambio de perfiles y campos dinámicos de inicio de sesión

--- CitasMed/Form1.Designer.cs
+++ CitasMed/Form1.Designer.cs
@@ -32,15 +32,15 @@
             tntTitulo = new Label();
             pictureBox1 = new PictureBox();
             linkLabel1 = new LinkLabel();
-            label1 = new Label();
+            lblPassword = new Label();
             textBox1 = new TextBox();
             button1 = new Button();
-            label3 = new LinkLabel();
-            label4 = new LinkLabel();
+            lblDoctor = new LinkLabel();
+            lblEmpleado = new LinkLabel();
             pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
             pictureBox4 = new PictureBox();
-            Label2 = new LinkLabel();
+            lblAdministrador = new LinkLabel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -49,23 +49,21 @@
             // 
             // tntTitulo
             // 
-            tntTitulo.AutoSize = true;
             tntTitulo.Font = new Font("Century Gothic", 40.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            tntTitulo.Location = new Point(498, 32);
-            tntTitulo.Margin = new Padding(4, 0, 4, 0);
+            tntTitulo.Location = new Point(285, 27);
             tntTitulo.Name = "tntTitulo";
-            tntTitulo.Size = new Size(313, 96);
+            tntTitulo.Size = new Size(470, 80);
             tntTitulo.TabIndex = 0;
-            tntTitulo.Text = "ADMIN";
+            tntTitulo.Text = "Asministrador";
+            tntTitulo.TextAlign = ContentAlignment.TopCenter;
             tntTitulo.Click += label1_Click;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(564, 170);
-            pictureBox1.Margin = new Padding(4);
+            pictureBox1.Location = new Point(426, 128);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(168, 165);
+            pictureBox1.Size = new Size(171, 168);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
@@ -73,77 +71,74 @@
             // linkLabel1
             // 
             linkLabel1.AutoSize = true;
-            linkLabel1.Location = new Point(638, 554);
-            linkLabel1.Margin = new Padding(4, 0, 4, 0);
+            linkLabel1.Location = new Point(510, 443);
             linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(0, 25);
+            linkLabel1.Size = new Size(0, 20);
             linkLabel1.TabIndex = 2;
             // 
-            // label1
+            // lblPassword
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Century Gothic", 22F);
-            label1.Location = new Point(415, 422);
-            label1.Margin = new Padding(4, 0, 4, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(490, 55);
-            label1.TabIndex = 3;
-            label1.Text = "Ingresa la contraseña";
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Century Gothic", 22F);
+            lblPassword.Location = new Point(332, 338);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(408, 44);
+            lblPassword.TabIndex = 3;
+            lblPassword.Text = "Ingresa la contraseña";
+            lblPassword.Click += lblPassword_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(356, 499);
-            textBox1.Margin = new Padding(4);
+            textBox1.Location = new Point(285, 399);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(602, 31);
+            textBox1.Size = new Size(482, 27);
             textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // button1
             // 
             button1.Font = new Font("Century Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(552, 568);
-            button1.Margin = new Padding(4);
+            button1.Location = new Point(408, 443);
             button1.Name = "button1";
-            button1.Size = new Size(215, 82);
+            button1.Size = new Size(215, 92);
             button1.TabIndex = 5;
             button1.Text = "Iniciar sesión";
             button1.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // lblDoctor
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.LinkColor = Color.Black;
-            label3.Location = new Point(59, 694);
-            label3.Margin = new Padding(4, 0, 4, 0);
-            label3.Name = "label3";
-            label3.Size = new Size(108, 32);
-            label3.TabIndex = 7;
-            label3.TabStop = true;
-            label3.Text = "DOCTOR";
+            lblDoctor.AutoSize = true;
+            lblDoctor.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDoctor.LinkColor = Color.Black;
+            lblDoctor.Location = new Point(47, 555);
+            lblDoctor.Name = "lblDoctor";
+            lblDoctor.Size = new Size(89, 28);
+            lblDoctor.TabIndex = 7;
+            lblDoctor.TabStop = true;
+            lblDoctor.Text = "DOCTOR";
+            lblDoctor.LinkClicked += lblDoctor_LinkClicked;
             // 
-            // label4
+            // lblEmpleado
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.LinkColor = Color.Black;
-            label4.Location = new Point(59, 749);
-            label4.Margin = new Padding(4, 0, 4, 0);
-            label4.Name = "label4";
-            label4.Size = new Size(134, 32);
-            label4.TabIndex = 8;
-            label4.TabStop = true;
-            label4.Text = "EMPLEADO";
+            lblEmpleado.AutoSize = true;
+            lblEmpleado.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblEmpleado.LinkColor = Color.Black;
+            lblEmpleado.Location = new Point(47, 599);
+            lblEmpleado.Name = "lblEmpleado";
+            lblEmpleado.Size = new Size(112, 28);
+            lblEmpleado.TabIndex = 8;
+            lblEmpleado.TabStop = true;
+            lblEmpleado.Text = "EMPLEADO";
+            lblEmpleado.LinkClicked += label4_LinkClicked;
             // 
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(16, 634);
-            pictureBox2.Margin = new Padding(4);
-            pictureBox2.MaximumSize = new Size(35, 35);
-            pictureBox2.MinimumSize = new Size(35, 35);
+            pictureBox2.Location = new Point(13, 507);
+            pictureBox2.MaximumSize = new Size(28, 28);
+            pictureBox2.MinimumSize = new Size(28, 28);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(35, 35);
+            pictureBox2.Size = new Size(28, 28);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 9;
             pictureBox2.TabStop = false;
@@ -151,12 +146,11 @@
             // pictureBox3
             // 
             pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(16, 749);
-            pictureBox3.Margin = new Padding(4);
-            pictureBox3.MaximumSize = new Size(35, 35);
-            pictureBox3.MinimumSize = new Size(35, 35);
+            pictureBox3.Location = new Point(13, 599);
+            pictureBox3.MaximumSize = new Size(28, 28);
+            pictureBox3.MinimumSize = new Size(28, 28);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(35, 35);
+            pictureBox3.Size = new Size(28, 28);
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox3.TabIndex = 10;
             pictureBox3.TabStop = false;
@@ -164,47 +158,45 @@
             // pictureBox4
             // 
             pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
-            pictureBox4.Location = new Point(16, 694);
-            pictureBox4.Margin = new Padding(4);
-            pictureBox4.MaximumSize = new Size(35, 35);
-            pictureBox4.MinimumSize = new Size(35, 35);
+            pictureBox4.Location = new Point(13, 555);
+            pictureBox4.MaximumSize = new Size(28, 28);
+            pictureBox4.MinimumSize = new Size(28, 28);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(35, 35);
+            pictureBox4.Size = new Size(28, 28);
             pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox4.TabIndex = 11;
             pictureBox4.TabStop = false;
             // 
-            // Label2
+            // lblAdministrador
             // 
-            Label2.AutoSize = true;
-            Label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Label2.LinkColor = Color.Black;
-            Label2.Location = new Point(59, 634);
-            Label2.Margin = new Padding(4, 0, 4, 0);
-            Label2.Name = "Label2";
-            Label2.Size = new Size(92, 32);
-            Label2.TabIndex = 12;
-            Label2.TabStop = true;
-            Label2.Text = "ADMIN";
+            lblAdministrador.AutoSize = true;
+            lblAdministrador.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblAdministrador.LinkColor = Color.Black;
+            lblAdministrador.Location = new Point(47, 507);
+            lblAdministrador.Name = "lblAdministrador";
+            lblAdministrador.Size = new Size(77, 28);
+            lblAdministrador.TabIndex = 12;
+            lblAdministrador.TabStop = true;
+            lblAdministrador.Text = "ADMIN";
+            lblAdministrador.LinkClicked += lblAdministrador_LinkClicked;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1271, 808);
-            Controls.Add(Label2);
+            ClientSize = new Size(1017, 646);
+            Controls.Add(lblAdministrador);
             Controls.Add(pictureBox4);
             Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
-            Controls.Add(label4);
-            Controls.Add(label3);
+            Controls.Add(lblEmpleado);
+            Controls.Add(lblDoctor);
             Controls.Add(button1);
             Controls.Add(textBox1);
-            Controls.Add(label1);
+            Controls.Add(lblPassword);
             Controls.Add(linkLabel1);
             Controls.Add(pictureBox1);
             Controls.Add(tntTitulo);
-            Margin = new Padding(4);
             Name = "Form1";
             Text = "ADMIN";
             Load += Form1_Load;
@@ -221,14 +213,14 @@
         private Label tntTitulo;
         private PictureBox pictureBox1;
         private LinkLabel linkLabel1;
-        private Label label1;
+        private Label lblPassword;
         private TextBox textBox1;
         private Button button1;
-        private LinkLabel label3;
-        private LinkLabel label4;
+        private LinkLabel lblDoctor;
+        private LinkLabel lblEmpleado;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
         private PictureBox pictureBox4;
-        private LinkLabel Label2;
+        private LinkLabel lblAdministrador;
     }
 }
