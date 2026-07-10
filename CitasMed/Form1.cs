3d504cc86665c895c8ceace36f8d277039321b5c
Lander Javier Serna Hernandez <sernalander11@gmail.com>
2026-07-09 22:11:36 -0600

    Se agregó cambio de perfiles y campos dinámicos de inicio de sesión

--- CitasMed/Form1.cs
+++ CitasMed/Form1.cs
@@ -1,3 +1,5 @@
+
+
 namespace CitasMed
 {
     public partial class Form1 : Form
@@ -5,11 +7,12 @@ namespace CitasMed
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -19,9 +22,42 @@ namespace CitasMed
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            tntTitulo.TextAlign = ContentAlignment.MiddleCenter;
+        }
+
+        private void lblAdministrador_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            tntTitulo.Text = "Administrador";
+            lblPassword.Visible = true;
+            textBox1.Visible = true;
+
+            lblPassword.Text = "ingrese contraseña";
+        }
+
+        private void label4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            tntTitulo.Text = "Empleado";
+            lblPassword.Visible = false;
+            textBox1.Visible = false;
 
         }
 
-     
+        private void lblDoctor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            tntTitulo.Text = "Doctor";
+            lblPassword.Visible=true; 
+            textBox1.Visible=true;
+            lblPassword.Text = "ingrese su usuario";
+        }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
