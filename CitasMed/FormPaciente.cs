8b9e1f634b271c48c69d059e3cbb90460dd71f26
Sangreazul_misti <joseluisfayad1825@gmail.com>
2026-07-17 20:08:16 -0600

    Correccion de base de datos de pacientes

--- CitasMed/FormPaciente.cs
+++ CitasMed/FormPaciente.cs
@@ -71,7 +71,19 @@ namespace CitasMed
                 {
                     conexion.Open();
 
-                    string consulta = "SELECT * FROM Paciente";
+                    string consulta = @"SELECT 
+                                id_paciente AS CLAVE,
+                                curp AS CURP,
+                                nombre AS NOMBRE,
+                                apellido_paterno AS 'APELLIDO PATERNO',
+                                apellido_materno AS 'APELLIDO MATERNO',
+                                telefono AS TELEFONO,
+                                calle AS CALLE,
+                                colonia AS COLONIA,
+                                municipio AS MUNICIPIO,
+                                entidad AS ENTIDAD,
+                                enfermedad_cronica AS 'ENFERMEDAD CRONICA'
+                                FROM Paciente";
 
                     MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, conexion);
                     DataTable tabla = new DataTable();
@@ -89,14 +101,6 @@ namespace CitasMed
             }
         }
 
-        private void btnRegresar_Click(object sender, EventArgs e)
-        {
-            FormEmpleado empleado = new FormEmpleado();
-            empleado.Show();
-            this.Hide();
-        }
-
-
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -106,6 +110,13 @@ namespace CitasMed
         private void FormPaciente_Load_1(object sender, EventArgs e)
         {
             CargarPacientes();
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            FormAdministrador admin = new FormAdministrador();
+            admin.Show();
+            this.Hide();
         }
 
     }
